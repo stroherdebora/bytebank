1b227eb959fb0c8d34d921db830ba9a0a4d59c31
Debora Stroher <debora.stroher@dwu.com.br>
2021-12-06 22:41:49 -0300

    handling snapshot states

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
-  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class BytebankApp extends StatelessWidget {
