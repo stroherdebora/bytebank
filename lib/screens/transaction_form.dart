4b9857752970dda44815724c058c35fcaec4f0d7
Debora Stroher <debora.stroher@dwu.com.br>
2021-12-21 11:42:50 -0300

    integrating authentication dialog

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
@@ -62,7 +63,16 @@ class _TransactionFormState extends State<TransactionForm> {
                     onPressed: () {
                       final double? value = double.tryParse(_valueController.text);
                       final transactionCreated = Transaction(value!, widget.contact);
-                      _webClient.save(transactionCreated).then((transaction) => Navigator.pop(context));
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return TransactionAuthDialog(
+                            onConfirm: (String password) {
+                              _webClient.save(transactionCreated, password).then((transaction) => Navigator.pop(context));
+                            },
+                          );
+                        },
+                      );
                     },
                   ),
                 ),
