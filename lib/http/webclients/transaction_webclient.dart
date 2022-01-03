a5280f5dc36ae53f4675209015718734589331b1
Debora Stroher <debora.stroher@dwu.com.br>
2022-01-03 17:13:14 -0300

    refactoring code

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -12,7 +12,7 @@ class TransactionWebClient {
     return decodedJson.map((dynamic json) => Transaction.fromJson(json)).toList();
   }
 
-  Future<Transaction> save(Transaction transaction, String password) async {
+  Future<Transaction?> save(Transaction transaction, String password) async {
     final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response? response = await client.post(Uri.http('192.168.13.122:8080', 'transactions'),
@@ -22,14 +22,20 @@ class TransactionWebClient {
         },
         body: transactionJson);
 
-    if (response!.statusCode == 400) {
-      throw Exception('there was an error submiting transaction');
+    if (response!.statusCode == 200) {
+      return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    if (response.statusCode == 401) {
-      throw Exception('authentication failed');
-    }
-
+    _throwHttpError(response.statusCode);
     return Transaction.fromJson(jsonDecode(response.body));
   }
+
+  void _throwHttpError(int statusCode) {
+    throw Exception(_statusCodeResponses[statusCode]);
+  }
+
+  static final Map<int, String> _statusCodeResponses = {
+    400: 'there was an error submiting transaction',
+    401: 'authentication failed',
+  };
 }
