11940504364c56fb60d283b337856d6b4e4f6476
Debora Stroher <debora.stroher@dwu.com.br>
2022-01-24 15:20:33 -0300

    debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,20 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
 
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    FirebaseCrashlytics.instance.setUserIdentifier('alura123');
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
+
   runApp(BytebankApp());
 }
 
