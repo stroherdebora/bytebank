e7b006e5c57c1dc55b959374bdacec261759c457
Debora Stroher <debora.stroher@dwu.com.br>
2022-03-24 08:57:53 -0300

    State Manager, UI, and Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:bytebank/components/theme.dart';
 import 'package:bytebank/models/balance.dart';
 import 'package:bytebank/models/transferencias.dart';
+import 'package:bytebank/screens/counter.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:bytebank/screens/dashboard/dashboard_states.dart';
 import 'package:flutter/material.dart';
@@ -39,15 +41,8 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
-      home: Dashboard(),
+      theme: bitebankTheme,
+      home: CounterContainer(),
     );
   }
 }
