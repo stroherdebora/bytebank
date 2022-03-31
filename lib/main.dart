634dce1e184757fee17b5e94c4720494cff81f2f
Debora Stroher <debora.stroher@dwu.com.br>
2022-03-31 20:09:48 -0300

    fix observer

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,19 @@ void main() async {
   }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
 }
 
-class LogObserver extends BlocObserver {
-  @override
-  void onChange(Cubit cubit, Change change) {
-    print("${cubit.runtimeType} > $change");
-    super.onChange(cubit, change);
-  }
-}
+// class LogObserver extends BlocObserver {
+//   @override
+//   void onChange(Cubit cubit, Change change) {
+//     print("${cubit.runtimeType} > $change");
+//     super.onChange(cubit, change);
+//   }
+// }
 
 class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Bloc.observer = LogObserver();
+
     return MaterialApp(
       theme: bitebankTheme,
       home: NameContainer(),
